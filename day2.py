5de45a298da6f1f3a6e3ad7e4f301947bfa5006f
brendes6 <143545666+brendes6@users.noreply.github.com>
2024-12-03 21:57:30 -0500

    add day 2

---
+++ day2.py
@@ -0,0 +1,32 @@
+
+def helper(vals):
+    # Checks all rules
+    # Check if list is ascending/descending and also if values differ between values 1-3
+    return (all(vals[i]<vals[i+1] for i in range(len(vals)-1)) or all(vals[i]>vals[i+1] for i in range(len(vals)-1))) and all(0 < abs(vals[i]-vals[i+1]) < 4 for i in range(len(vals)-1))
+
+
+def question_1_2(filename, skip_allowed):
+    count = 0
+
+    with open(filename, "r") as f:
+        for line in f:
+            vals = [int(i) for i in line.split()]
+
+            #check if vals passes rule set
+            if helper(vals):
+                count += 1
+            
+            # If vals doesnt pass, and skip is allowed, then:
+            elif skip_allowed:
+                
+                # for each possible removal in vals, check if removing it passes the rule set, if so increment count and break
+                for i in range(len(vals)):
+                    if helper([v for j, v in enumerate(vals) if j!=i]):
+                        count += 1
+                        break
+    
+    return count
+
+if __name__ == "__main__":
+    print("Part one solution: ", question_1_2("input_day2.txt", False))
+    print("Part one solution: ", question_1_2("input_day2.txt", True))
